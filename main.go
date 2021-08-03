0e4db254624879f2dd811ea83a8e5392074ec339
Karl Hepworth <karl.hepworth@gmail.com>
2021-08-03 11:49:50 +1000

    Updates
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -17,9 +19,32 @@ const (
 	CONFIG_PASSWORD = "PROXY_APP_CONFIG_KEY_PASSWORD"
 )
 
+var (
+	ProxyAppUserName string
+	ProxyAppPassword string
+)
+
 func main() {
 	Run()
 }
+
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
+	fmt.Printf("%v", r.Header.Get("Authorization"))
+	return f(r)
+}
+
+func NewRoundTripper(original http.RoundTripper) http.RoundTripper {
+	if original == nil {
+		original = http.DefaultTransport
+	}
+	return roundTripperFunc(func(request *http.Request) (*http.Response, error) {
+		request.Header.Set("Authorization", fmt.Sprintf("Basic %s:%s", ProxyAppUserName, ProxyAppPassword))
+		response, err := original.RoundTrip(request)
+		return response, err
+	})
+}
+
 
 func Run() {
 
@@ -29,39 +54,48 @@ func Run() {
 	//	panic(err)
 	//}
 	ProxyAppEndpoint := skprclient.GetWithFallback(os.Getenv(CONFIG_ENDPOINT), os.Getenv("PROXY_APP_ENDPOINT"))
-	// @TODO.
-	// ProxyAppUserName := skprclient.GetWithFallback(os.Getenv(CONFIG_USERNAME), os.Getenv("PROXY_APP_USERNAME"))
-	// @TODO.
-	// ProxyAppPassword := skprclient.GetWithFallback(os.Getenv(CONFIG_PASSWORD), os.Getenv("PROXY_APP_PASSWORD"))
+	ProxyAppUserName = skprclient.GetWithFallback(os.Getenv(CONFIG_USERNAME), os.Getenv("PROXY_APP_USERNAME"))
+	ProxyAppPassword = skprclient.GetWithFallback(os.Getenv(CONFIG_PASSWORD), os.Getenv("PROXY_APP_PASSWORD"))
 
 	ProxyAppPort := os.Getenv("PROXY_APP_PORT")
-	if ProxyAppPort == "" {
-		ProxyAppPort = ":8080"
-	}
 
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
-	// Setup proxy
-	url, err := url.Parse(ProxyAppEndpoint)
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-	targets := []*middleware.ProxyTarget{
-		{
-			URL:  url,
-		},
+	var targets []*middleware.ProxyTarget
+	for _, t := range strings.Split(ProxyAppEndpoint, ",") {
+		// Setup proxy
+		url, err := url.Parse(t)
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+		targets = append(targets, &middleware.ProxyTarget{
+			URL: url,
+		})
 	}
 
-	// Debug messaging.
+	rrb := middleware.NewRoundRobinBalancer(targets)
+	var rt http.RoundTripper
+	rt = NewRoundTripper(rt)
+
+	ProxyConfig := &middleware.ProxyConfig{
+		Skipper:    nil,
+		Balancer:   rrb,
+		Rewrite:    nil,
+		ContextKey: "target",
+		Transport: rt,
+	}
+
+	// Debug messaging - also add target to LB.
 	fmt.Printf("Proxy configured to use port %s\n", ProxyAppPort)
 	for _, target := range targets {
+		ProxyConfig.Balancer.AddTarget(target)
 		fmt.Printf("Starting proxy on http://localhost%s for endpoint %s\n", ProxyAppPort, target.URL)
 	}
 
 	// Start serving the proxy as configured.
-	e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
+	e.Use(middleware.ProxyWithConfig(*ProxyConfig))
 	e.Logger.Fatal(e.Start(ProxyAppPort))
 
 }
